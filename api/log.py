ce97bec5d520fecf00b71ff9c63b687cc5d86db6
bsrtgdsgts-cmyk <bsrtgdsgts@gmail.com>
2026-04-19 11:17:52 -0400

    Update log.py

--- api/log.py
+++ api/log.py
@@ -1,1 +1,17 @@
+from http.server import BaseHTTPRequestHandler
+import json
 
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        
+        # Log the received data to the console
+        print("--- DATA RECEIVED ---")
+        print(post_data.decode('utf-8'))
+        print("---------------------")
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({'status': 'success'}).encode('utf-8'))
